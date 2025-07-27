1c2e6ff78c836e2b3542d42525b9408c6f8394b6
HoangQuocDuy29 <quocduyhoang42@gmail.com>
2025-07-27 15:51:47 +0700

    v5 - Middleware 404 Not Found

--- vite-project/src/App.tsx
+++ vite-project/src/App.tsx
@@ -1,12 +1,21 @@
+import { Routes, Route } from "react-router-dom";
 import HomePage from "@/pages/HomePage";
+import NotFoundPage from "@/pages/NotFoundPage";
 
 function App() {
   return (
     <div className="min-h-screen bg-gray-100 p-6">
       <h1 className="text-3xl font-bold text-center text-blue-600 mb-4">
-        Task Manager App
+        Task Manager
       </h1>
-      <HomePage />
+
+      <Routes>
+        {/* ✅ Route chính */}
+        <Route path="/" element={<HomePage />} />
+
+        {/* ❌ Route không tồn tại → Trang 404 */}
+        <Route path="*" element={<NotFoundPage />} />
+      </Routes>
     </div>
   );
 }
